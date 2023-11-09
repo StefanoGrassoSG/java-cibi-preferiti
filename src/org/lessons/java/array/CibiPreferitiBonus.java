62d1897f1b7ffcb47a301655bf7eb992ac49861f
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-11-09 17:20:21 +0100

    complete with bonus

--- src/org/lessons/java/array/CibiPreferitiBonus.java
+++ src/org/lessons/java/array/CibiPreferitiBonus.java
@@ -1,20 +1,20 @@
 package org.lessons.java.array;
 
-public class CibiPreferiti {
+public class CibiPreferitiBonus {
 	
 	public static void main(String[] args) {
-		
-		String[] myBestFoods = {"pizza", "ravioli", "lasagne", "carbonara", "prosciutto"};
+			
+		String[] myBestFoods = {"pizza", "ravioli", "lasagne", "carbonara", "prosciutto", "melone", "formaggio", "verdure", "pollo", "patatine"};
 		
 		String firstFood = myBestFoods[0];
 		String lastFood = myBestFoods[myBestFoods.length - 1];
 		int index = myBestFoods.length / 2;
 		String food = myBestFoods[index];
+		String food2 = myBestFoods[index - 1];
 		
 		System.out.println("la quantità dei miei cibi preferiti è: " + myBestFoods.length);
 		System.out.println("il primo è : " + firstFood);
 		System.out.println("l'ultimo è : " + lastFood);
-		System.out.println("quello in mezzo è : " + food);
-	}
+		System.out.println("gli elementi in mezzo sono : " + food + " e " + food2);
+		}
 }
-
